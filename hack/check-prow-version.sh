25ed6e591fa0fff39fd9b761093b0e3d740a8fa5
Nikolaos Moraitis <nmoraiti@redhat.com>
2026-04-16 14:37:05 +0300

    remove the vendor folder
    Signed-off-by: Nikolaos Moraitis <nmoraiti@redhat.com>

--- hack/check-prow-version.sh
+++ hack/check-prow-version.sh
@@ -5,40 +5,57 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-# This script checks to make sure that the vendored version of the kubernetes-sigs/prow repo here is
-# no newer than those vendored into the release controller and chat bot. We assume a layout
-# of repositories as we get from Prow's pod utilities.
+# Ensures sigs.k8s.io/prow resolves to the same module version in ci-tools,
+# release-controller, and ci-chat-bot.
+#
+# With sibling clones (../release-controller, ../ci-chat-bot), uses go list.
+# Otherwise fetches go.mod from GitHub for the peer branch (PULL_BASE_REF or main).
 
-function determine_vendored_commit() {
+function prow_module_line_from_dir() {
 	local dir="$1"
-	pushd "${dir}" >/dev/null
-	local version
-	# this will be of the form v0.0.0-20210115214543-aefe406fe7b6
-	version=$( go list -mod=mod -m sigs.k8s.io/prow )
-	local commit
-	# this extracts aefe406fe7b6
-	commit="${version##*-}"
-	popd >/dev/null
-	echo "${commit}"
+	(
+		cd "${dir}"
+		go list -mod=mod -m sigs.k8s.io/prow
+	)
 }
 
+function prow_module_line_from_github() {
+	local orgrepo="$1"
+	local branch="${2:-main}"
+	local url="https://raw.githubusercontent.com/${orgrepo}/${branch}/go.mod"
+	local modfile
+	modfile="$( mktemp )"
+	curl -fsSL "${url}" -o "${modfile}"
+	local line
+	line="$( grep -E '^[[:space:]]*sigs\.k8s\.io/prow[[:space:]]+v' "${modfile}" | grep -v '^replace' | head -1 )" || true
+	rm -f "${modfile}"
+	if [[ -z "${line}" ]]; then
+		echo "[FATAL] could not find sigs.k8s.io/prow require in ${url}" >&2
+		return 1
+	fi
+	echo "sigs.k8s.io/prow $( echo "${line}" | awk '{print $2}' )"
+}
 
-ci_tools_vendored_commit="$( determine_vendored_commit . )"
-release_controller_vendored_commit="$( determine_vendored_commit ./../release-controller )"
-ci_chat_bot_vendored_commit="$( determine_vendored_commit ./../ci-chat-bot )"
+function prow_module_line_peer() {
+	local orgrepo="$1"
+	local sibling_dir="$2"
+	local branch="${PULL_BASE_REF:-main}"
 
-failures=0
+	if [[ -f "${sibling_dir}/go.mod" ]]; then
+		prow_module_line_from_dir "${sibling_dir}"
+	else
+		prow_module_line_from_github "${orgrepo}" "${branch}"
+	fi
+}
 
-pushd ./../../kubernetes-sigs/prow
-if ! git merge-base --is-ancestor "${ci_tools_vendored_commit}" "${release_controller_vendored_commit}"; then
-	echo "[FATAL] The release-controller repo vendors prow at ${release_controller_vendored_commit}, which is older than the ci-tools vendor at ${ci_tools_vendored_commit}"
-	failures=$((failures+1))
+ci_tools_version="$( prow_module_line_from_dir . )"
+release_controller_version="$( prow_module_line_peer openshift/release-controller "${PWD}/../release-controller" )"
+ci_chat_bot_version="$( prow_module_line_peer openshift/ci-chat-bot "${PWD}/../ci-chat-bot" )"
+
+if [[ "${ci_tools_version}" != "${release_controller_version}" ]] || [[ "${ci_tools_version}" != "${ci_chat_bot_version}" ]]; then
+	echo "[FATAL] sigs.k8s.io/prow must match across ci-tools, release-controller, and ci-chat-bot."
+	echo "  ci-tools:            ${ci_tools_version}"
+	echo "  release-controller:  ${release_controller_version}"
+	echo "  ci-chat-bot:         ${ci_chat_bot_version}"
+	exit 1
 fi
-
-if ! git merge-base --is-ancestor "${ci_tools_vendored_commit}" "${ci_chat_bot_vendored_commit}"; then
-	echo "[FATAL] The ci-chat-bot repo vendors prow at ${ci_chat_bot_vendored_commit}, which is older than the ci-tools vendor at ${ci_tools_vendored_commit}"
-	failures=$((failures+1))
-fi
-popd
-
-exit $failures
